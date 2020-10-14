f96e894c0b01503c3feb07a40f754b41308399ad
Nikita Tushin <spbtne@gmail.com>
2020-10-14 20:27:49 +0300

    fix: improve solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,13 +3,18 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(matrix) {
    
   let result = matrix.reduce(function(counter,item,index, array){
-    for (let i = 0; i < item.length; i +=1)
-      {
-    if(item[i] === '^^') {
-      counter +=1;
-    } else {counter}
-    }
+    item.forEach(element => element === '^^' ? counter +=1 : counter)
     return counter;
+      
+  //   for (let i = 0; i < item.length; i +=1)
+  //     {
+  
+  //   if(item[i] === '^^') {
+  //     counter +=1;
+  //   } 
+  //   }
+  //   return counter;
+  
   }, 0);
 return result;
   // remove line with error and write your code here
