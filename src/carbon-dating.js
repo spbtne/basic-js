76beabc04749cce6d0bc6dd0e7623fc4f736b320
Nikita Tushin <spbtne@gmail.com>
2020-10-14 21:50:05 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,16 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  throw new CustomError('Not implemented');
-  
+  let numActivity;
+  if (typeof sampleActivity === 'string')  { 
+    if (sampleActivity.length > 0) {
+    let numActivity = Math.ceil(Number(sampleActivity))}
+    if (numActivity == undefined ) { return false
+
+    }
+  }
+else { return false};
+let k = 0.693/HALF_LIFE_PERIOD;
+let t = Math.log2(MODERN_ACTIVITY/numActivity) / k;
+  return t;
 };
