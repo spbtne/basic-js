2591ea3f5a257e7ffa2189ab38be643776652ca6
Nikita Tushin <spbtne@gmail.com>
2020-10-16 13:15:41 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let mainText ='';
+  if (str === null) { 
+   mainText = 'null';
+ } else {
+ mainText = str.toString();
+}
+  let result = mainText;
+
+  if (options.separator === undefined) {
+     Object.defineProperty(options, 'separator',{ 
+    value: '+'
+  })
+}
+
+ if (options.additionSeparator === undefined) {
+  Object.defineProperty(options, 'additionSeparator',{ 
+    value: '|'
+  })
+}
+
+  if (options === undefined) { 
+    return result;
+  }// Если задана только строка
+
+
+ if (options.addition === undefined) {
+  for (let i = 1; i <= options.repeatTimes - 1; i += 1) {
+    result = result + options.separator + mainText;
+  }
+  return result;   
+}//Задана строка и количество её повторений 
+
+if (options.repeatTimes === undefined) {
+  result = result + options.addition;
+  for (let i = 1; i < options.additionRepeatTimes; i += 1) {
+    result = result + options.additionSeparator + options.addition;
+  }
+  return result;
+  }//Не задано количество повторений для основного текста
+
+if (options.additionRepeatTimes === undefined) {
+  for (let i = 1; i <= options.repeatTimes - 1; i += 1) {
+      result = result + options.additionSeparator + options.addition + options.separator + mainText;
+  }
+  return result; //Не задано количество повторений для дополнения
+}
+
+for (let i = 1; i < options.repeatTimes; i += 1) {
+  result = result + options.addition;
+  for (let y = 1; y < options.additionRepeatTimes; y +=1) {
+    result = result + options.additionSeparator + options.addition;
+  }
+   result = result + options.separator + mainText
+}
+result = result + options.addition;
+for (let y = 1; y < options.additionRepeatTimes; y +=1 ) {
+  result = result + options.additionSeparator + options.addition;
+  }
+return result; //заданы все параметры
+
+
 };
   
