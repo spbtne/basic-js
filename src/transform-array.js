0f790b8bc5ea9164711a7c32468b405994640847
Nikita Tushin <spbtne@gmail.com>
2020-11-01 18:40:03 +0300

    add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,26 +1,27 @@
+
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  throw new CustomError('Not implemented');
-
-  if (!Array.isArray(arr)) {
-    throw new Error('Not array!');
-  }
-  let result = [];
-  for (let i = 0; i < arr.length; ++i) {
-    if (arr[i] === '--double-next' && i + 1 < arr.length) {
-      result.push(arr[i + 1]);
-    } else if (arr[i] === '--discard-next' && i + 1 < arr.length) {
-      i += 2;
-    } else if (arr[i] === '--double-prev' && i - 1 >= 0) {
-      result.push(arr[i - 1]);
-    } else if (arr[i] === '--discard-prev' && i - 1 >= 0) {
-      result.pop();
-    } else {
-      result.push(arr[i]);
+  const result = [];
+  if (!(arr instanceof Array)) throw new Error();
+  for (let i = 0; i < arr.length; i++)
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev' :
+        if (arr[i - 2] !== '--discard-next')
+          result.pop();
+        break;
+      case '--double-next' :
+        i !== arr.length - 1 ? result.push(arr[i + 1]) : null;
+        break;
+      case '--double-prev' :
+        if (arr[i - 2] !== '--discard-next')
+          i !== 0 ? result.push(arr[i - 1]) : null;
+        break;
+      default:
+        result.push(arr[i])
     }
-  }
-  return result.filter(x => x !== '--discard-next' &&
-    x !== '--discard-prev' && x !== '--double-next' &&
-    x !== '--double-prev');
-};
+  return result
+}
